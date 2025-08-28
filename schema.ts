16a48fb2bf77c7b437d23590cd9a74fc43d25842
Roscoe Rubin-Rottenberg <roscoe@knotbin.com>
2025-08-27 23:38:11 -0400

    zod to standard schema

--- schema.ts
+++ schema.ts
@@ -1,10 +1,16 @@
-import type { z } from "zod";
+import type { StandardSchemaV1 } from "@standard-schema/spec";
 import type { ObjectId } from "mongodb";
 
-export type InferModel<T extends z.ZodObject> = z.infer<T> & {
-  _id?: ObjectId;
-};
+export type InferModel<
+  T extends StandardSchemaV1<unknown, Record<string, unknown>>,
+> =
+  & StandardSchemaV1.InferOutput<T>
+  & {
+    _id?: ObjectId;
+  };
 
-export type InsertType<T extends z.ZodObject> =
-  & Omit<z.infer<T>, "createdAt">
+export type InsertType<
+  T extends StandardSchemaV1<unknown, Record<string, unknown>>,
+> =
+  & Omit<StandardSchemaV1.InferOutput<T>, "createdAt">
   & { createdAt?: Date };
